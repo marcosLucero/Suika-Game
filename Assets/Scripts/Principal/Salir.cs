9ca0d5c57cfe83161a4bce3d5c5450aa5ed9dd80
marcosLucero <marcoslucero@ceroca.cat>
2025-01-17 13:22:54 +0100

    Botones Scripts
    boton entrar y salir funcionales

---
+++ Assets/Scripts/Principal/Salir.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Salir : MonoBehaviour
+{
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
