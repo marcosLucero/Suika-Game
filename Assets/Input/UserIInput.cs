2503e0ac45712a70f77de6e96a2c26f45b5c3241
marcosLucero <marcoslucero@ceroca.cat>
2025-01-19 23:30:04 +0100

    Video Empezado
    Ruleta con minerales burbujas, contenedor de los minerales, animacion de puntacion y siguiente mineral
    
    Min 2:32
    https://youtu.be/liDKiD6yv8E?si=1olJ-H5SjAaRhyjb&t=152

---
+++ Assets/Input/UserIInput.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class UserIInput : MonoBehaviour
+{
+    public static PlayerInput PlayerInput;
+
+}
