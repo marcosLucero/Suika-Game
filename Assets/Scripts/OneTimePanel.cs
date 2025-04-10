421ab8e687ac37e7d168f677fdbb3ed32baf4fb1
marcosLucero <marcoslucero@ceroca.cat>
2025-04-10 16:47:26 +0200

    US16 ScenaGuia
    Añadido a las escenas de juego una guia de controles

---
+++ Assets/Scripts/OneTimePanel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasOncePerSession : MonoBehaviour
+{
+    [SerializeField] private Canvas targetCanvas;
+    [SerializeField] private Button closeButton;
+    private static bool canvasShown = false;
+
+    private void Start()
+    {
+        if (!canvasShown)
+        {
+            targetCanvas.gameObject.SetActive(true);
+            Time.timeScale = 0f; // ⏸️ Pausar el juego
+            canvasShown = true;
+
+            if (closeButton != null)
+            {
+                closeButton.onClick.AddListener(HideCanvas);
+            }
+        }
+        else
+        {
+            targetCanvas.gameObject.SetActive(false);
+        }
+    }
+
+    public void HideCanvas()
+    {
+        targetCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1f; // ▶️ Reanudar el juego
+    }
+}
