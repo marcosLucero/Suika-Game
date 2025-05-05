3ee95afbd8e0d76929da9c0dfdfb67834608e49e
marcosLucero <marcoslucero@ceroca.cat>
2025-05-05 16:25:55 +0200

    US17 PullirGraficos
    cambio en los botones del inpuct action editor

---
+++ Assets/Input/CanvasSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem; // Solo si usas el nuevo Input System
+
+public class CanvasSwitcher : MonoBehaviour
+{
+    public GameObject primerCanvas;
+    public GameObject botonPorSeleccionar; // botón del segundo canvas
+
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            primerCanvas.SetActive(false);
+
+            // ✳️ Forzar selección tras ocultar el primer canvas
+            EventSystem.current.SetSelectedGameObject(null); // limpiar selección anterior
+            EventSystem.current.SetSelectedGameObject(botonPorSeleccionar);
+        }
+    }
+}
